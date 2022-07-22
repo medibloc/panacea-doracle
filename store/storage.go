875663c8bcf34bb2c8098fcbbd48d3460a0074bf
hansol-medi <hansol@medibloc.org>
2022-07-22 10:26:31 +0900

    change storage interface

--- store/storage.go
+++ store/storage.go
@@ -2,7 +2,5 @@ package store
 
 type Storage interface {
 	UploadFile(path, name string, data []byte) error
-	MakeDownloadURL(path, name string) string
-	MakeRandomFilename() string
 	DownloadFile(path, name string) ([]byte, error)
 }

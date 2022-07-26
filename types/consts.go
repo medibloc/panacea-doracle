5341f2945bd0d1ca50e44c1871636c92764df5f6
hansol-medi <hansol@medibloc.org>
2022-07-26 16:42:51 +0900

    fix

--- types/consts.go
+++ types/consts.go
@@ -1,18 +1,8 @@
 package types
 
-import "path/filepath"
-
 var (
-	DefaultDataDir    = "data"
-	DefaultDoracleDir = ".doracle"
-	DefaultConfigDir  = filepath.Join(DefaultDataDir, DefaultDoracleDir)
-
 	DefaultOraclePrivKeyName = "oracle_priv_key.sealed"
 	DefaultNodePrivKeyName   = "node_priv_key.sealed"
 
 	DefaultOraclePubKeyName = "oracle_pub_key.json"
-
-	DefaultNodePrivKeyFilePath   = filepath.Join(DefaultConfigDir, DefaultNodePrivKeyName)
-	DefaultOraclePrivKeyFilePath = filepath.Join(DefaultConfigDir, DefaultOraclePrivKeyName)
-	DefaultOraclePubKeyFilePath  = filepath.Join(DefaultConfigDir, DefaultOraclePubKeyName)
 )

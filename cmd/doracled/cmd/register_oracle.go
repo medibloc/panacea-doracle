adda53a5c1dbf4220d0dca7e6099a0f0bd1ba9b4
hansol-medi <hansol@medibloc.org>
2022-08-22 16:23:51 +0900

    remove arg of cmd in creating new service

--- cmd/doracled/cmd/register_oracle.go
+++ cmd/doracled/cmd/register_oracle.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"encoding/base64"
 	"fmt"
 	"github.com/medibloc/panacea-doracle/client/flags"
 	"github.com/medibloc/panacea-doracle/config"
+	"github.com/medibloc/panacea-doracle/panacea"
 	"github.com/medibloc/panacea-doracle/server/regoracle"
 	"github.com/medibloc/panacea-doracle/server/service"
 	"github.com/spf13/cobra"
@@ -30,7 +32,21 @@ func RegisterOracleCmd() *cobra.Command {
 				return fmt.Errorf("failed to init logger: %w", err)
 			}
 
-			svc, err := service.New(cmd, conf)
+			// get trusted block information
+			trustedBlockInfo, err := getTrustedBlockInfo(cmd)
+			if err != nil {
+				log.Errorf("failed to get trusted block info: %v", err)
+				return fmt.Errorf("failed to get trusted block info: %w", err)
+			}
+
+			// get oracle account from mnemonic.
+			oracleAccount, err := getOracleAccount(cmd, conf.OracleMnemonic)
+			if err != nil {
+				log.Errorf("failed to get oracle account from mnemonic: %v", err)
+				return fmt.Errorf("failed to get oracle account from mnemonic: %w", err)
+			}
+
+			svc, err := service.New(conf, oracleAccount, trustedBlockInfo)
 			if err != nil {
 				log.Errorf("failed to create a new service: %v", err)
 				return err
@@ -79,4 +95,56 @@ func RegisterOracleCmd() *cobra.Command {
 	_ = cmd.MarkFlagRequired(flags.FlagTrustedBlockHash)
 
 	return cmd
+}
+
+// getTrustedBlockInfo gets trusted block height and hash from cmd flags
+func getTrustedBlockInfo(cmd *cobra.Command) (*panacea.TrustedBlockInfo, error) {
+	trustedBlockHeight, err := cmd.Flags().GetInt64(flags.FlagTrustedBlockHeight)
+	if err != nil {
+		return nil, err
+	}
+	if trustedBlockHeight == 0 {
+		return nil, fmt.Errorf("trusted block height cannot be zero")
+	}
+
+	trustedBlockHashStr, err := cmd.Flags().GetString(flags.FlagTrustedBlockHash)
+	if err != nil {
+		return nil, err
+	}
+	if trustedBlockHashStr == "" {
+		return nil, fmt.Errorf("trusted block hash cannot be empty")
+	}
+
+	trustedBlockHash, err := base64.StdEncoding.DecodeString(trustedBlockHashStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &panacea.TrustedBlockInfo{
+		TrustedBlockHeight: trustedBlockHeight,
+		TrustedBlockHash:   trustedBlockHash,
+	}, nil
+}
+
+// getOracleAccount gets an oracle account from mnemonic.
+// The account is equal to one that is registered as validator.
+// You can set account number and index optionally.
+// The default value is 0 for both account number and index
+func getOracleAccount(cmd *cobra.Command, mnemonic string) (*panacea.OracleAccount, error) {
+	accNum, err := cmd.Flags().GetUint32(flags.FlagAccNum)
+	if err != nil {
+		return &panacea.OracleAccount{}, err
+	}
+
+	index, err := cmd.Flags().GetUint32(flags.FlagIndex)
+	if err != nil {
+		return &panacea.OracleAccount{}, err
+	}
+
+	oracleAccount, err := panacea.NewOracleAccount(mnemonic, accNum, index)
+	if err != nil {
+		return &panacea.OracleAccount{}, err
+	}
+
+	return oracleAccount, nil
 }
--- server/service/service.go
+++ server/service/service.go
@@ -1,98 +1,32 @@
 package service
 
 import (
-	"encoding/base64"
 	"fmt"
-	"github.com/medibloc/panacea-doracle/client/flags"
 	"github.com/medibloc/panacea-doracle/config"
 	"github.com/medibloc/panacea-doracle/panacea"
-	"github.com/spf13/cobra"
 )
 
 type Service struct {
-	Conf          *config.Config
-	OracleAccount *panacea.OracleAccount
-	TrustedHeight int64
-	TrustedHash   []byte
-	PanaceaClient panacea.GrpcClientI
+	Conf             *config.Config
+	OracleAccount    *panacea.OracleAccount
+	TrustedBlockInfo *panacea.TrustedBlockInfo
+	PanaceaClient    panacea.GrpcClientI
 }
 
-func New(cmd *cobra.Command, conf *config.Config) (*Service, error) {
-	// get trusted block information
-	height, hash, err := getTrustedBlockInfo(cmd)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get trusted block info: %w", err)
-	}
-
-	// get oracle account from mnemonic.
-	oracleAccount, err := getOracleAccount(cmd, conf.OracleMnemonic)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get oracle account from mnemonic: %w", err)
-	}
-
+func New(conf *config.Config, oracleAccount *panacea.OracleAccount, trustedBlockInfo *panacea.TrustedBlockInfo) (*Service, error) {
 	panaceaClient, err := panacea.NewGrpcClient(conf)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create Panacea gRPC client: %w", err)
 	}
 
 	return &Service{
-		Conf:          conf,
-		OracleAccount: oracleAccount,
-		TrustedHeight: height,
-		TrustedHash:   hash,
-		PanaceaClient: panaceaClient,
+		Conf:             conf,
+		OracleAccount:    oracleAccount,
+		TrustedBlockInfo: trustedBlockInfo,
+		PanaceaClient:    panaceaClient,
 	}, nil
 }
 
 func (svc *Service) Close() {
 	_ = svc.PanaceaClient.Close()
-}
-
-// getTrustedBlockInfo gets trusted block height and hash from cmd flags
-func getTrustedBlockInfo(cmd *cobra.Command) (int64, []byte, error) {
-	trustedBlockHeight, err := cmd.Flags().GetInt64(flags.FlagTrustedBlockHeight)
-	if err != nil {
-		return 0, nil, err
-	}
-	if trustedBlockHeight == 0 {
-		return 0, nil, fmt.Errorf("trusted block height cannot be zero")
-	}
-
-	trustedBlockHashStr, err := cmd.Flags().GetString(flags.FlagTrustedBlockHash)
-	if err != nil {
-		return 0, nil, err
-	}
-	if trustedBlockHashStr == "" {
-		return 0, nil, fmt.Errorf("trusted block hash cannot be empty")
-	}
-
-	trustedBlockHash, err := base64.StdEncoding.DecodeString(trustedBlockHashStr)
-	if err != nil {
-		return 0, nil, err
-	}
-
-	return trustedBlockHeight, trustedBlockHash, nil
-}
-
-// getOracleAccount gets an oracle account from mnemonic.
-// The account is equal to one that is registered as validator.
-// You can set account number and index optionally.
-// The default value is 0 for both account number and index
-func getOracleAccount(cmd *cobra.Command, mnemonic string) (*panacea.OracleAccount, error) {
-	accNum, err := cmd.Flags().GetUint32(flags.FlagAccNum)
-	if err != nil {
-		return &panacea.OracleAccount{}, err
-	}
-
-	index, err := cmd.Flags().GetUint32(flags.FlagIndex)
-	if err != nil {
-		return &panacea.OracleAccount{}, err
-	}
-
-	oracleAccount, err := panacea.NewOracleAccount(mnemonic, accNum, index)
-	if err != nil {
-		return &panacea.OracleAccount{}, err
-	}
-
-	return oracleAccount, nil
 }

3d8dd954b9a60cbe986b9267e2c6dc3903945c5b
Hansol Lee <38912532+H4NLee@users.noreply.github.com>
2022-08-25 11:32:10 +0900

    feat: implementation of register-oracle cmd (#11)

---
+++ crypto/key.go
@@ -0,0 +1,25 @@
+package crypto
+
+import (
+	"fmt"
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/go-bip39"
+	"github.com/tendermint/tendermint/crypto/secp256k1"
+)
+
+// NewPrivKey generates a random secp256k1 private key
+func NewPrivKey() (*btcec.PrivateKey, error) {
+	return btcec.NewPrivateKey(btcec.S256())
+}
+
+func GeneratePrivateKeyFromMnemonic(mnemonic string, coinType, accNum, index uint32) (secp256k1.PrivKey, error) {
+	if !bip39.IsMnemonicValid(mnemonic) {
+		return nil, fmt.Errorf("invalid mnemonic")
+	}
+
+	hdPath := hd.NewFundraiserParams(accNum, coinType, index).String()
+	master, ch := hd.ComputeMastersFromSeed(bip39.NewSeed(mnemonic, ""))
+
+	return hd.DerivePrivateKeyForPath(master, ch, hdPath)
+}

5ee1687d449fdba6458fd5751e8c9cf18e4e096a
audtlr24 <audtlr24@gmail.com>
2022-09-08 14:05:47 +0900

    feat: add GetOracleRegistration query using light client (#40)
    Co-authored-by: gyuguen <gyuguen.jang@medibloc.org>
    Co-authored-by: Youngjoon Lee <taxihighway@gmail.com>

--- cmd/doracled/cmd/gen_oracle_key.go
+++ cmd/doracled/cmd/gen_oracle_key.go
@@ -22,59 +22,62 @@ type OraclePubKeyInfo struct {
 	RemoteReportBase64 string `json:"remote_report_base64"`
 }
 
-var genOracleKeyCmd = &cobra.Command{
-	Use:   "gen-oracle-key",
-	Short: "Generate oracle key and its remote report",
-	Long: `Generate a new pair of oracle key and its remote report. 
+func genOracleKeyCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "gen-oracle-key",
+		Short: "Generate oracle key and its remote report",
+		Long: `Generate a new pair of oracle key and its remote report. 
 If the sealed oracle private key exist already, this command will replace the existing one.
 So please be cautious in using this command.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		conf, err := loadConfigFromHome(cmd)
-		if err != nil {
-			return err
-		}
-
-		// If there is the existing oracle key, double-check for generating a new oracle key
-		oraclePrivKeyPath := conf.AbsOraclePrivKeyPath()
-		if tos.FileExists(oraclePrivKeyPath) {
-			buf := bufio.NewReader(os.Stdin)
-			ok, err := input.GetConfirmation("This can replace the existing oracle-key.sealed file.\nAre you sure to make a new oracle key?", buf, os.Stderr)
-
-			if err != nil || !ok {
-				log.Printf("Oracle key generation is canceled.")
+		RunE: func(cmd *cobra.Command, args []string) error {
+			conf, err := loadConfigFromHome(cmd)
+			if err != nil {
 				return err
 			}
-		}
 
-		// generate a new oracle key
-		oraclePrivKey, err := crypto.NewPrivKey()
-		if err != nil {
-			log.Errorf("failed to generate oracle key: %v", err)
-			return err
-		}
+			// If there is the existing oracle key, double-check for generating a new oracle key
+			oraclePrivKeyPath := conf.AbsOraclePrivKeyPath()
+			if tos.FileExists(oraclePrivKeyPath) {
+				buf := bufio.NewReader(os.Stdin)
+				ok, err := input.GetConfirmation("This can replace the existing oracle-key.sealed file.\nAre you sure to make a new oracle key?", buf, os.Stderr)
 
-		// seal and store oracle private key
-		if err := sgx.SealToFile(oraclePrivKey.Serialize(), oraclePrivKeyPath); err != nil {
-			log.Errorf("failed to write %s: %v", oraclePrivKeyPath, err)
-			return err
-		}
+				if err != nil || !ok {
+					log.Printf("Oracle key generation is canceled.")
+					return err
+				}
+			}
 
-		// generate oracle key remote report
-		oraclePubKey := oraclePrivKey.PubKey().SerializeCompressed()
-		oracleKeyRemoteReport, err := sgx.GenerateRemoteReport(oraclePubKey)
-		if err != nil {
-			log.Errorf("failed to generate remote report of oracle key: %v", err)
-			return err
-		}
+			// generate a new oracle key
+			oraclePrivKey, err := crypto.NewPrivKey()
+			if err != nil {
+				log.Errorf("failed to generate oracle key: %v", err)
+				return err
+			}
 
-		// store oracle pub key and its remote report to a file
-		if err := storeOraclePubKey(oraclePubKey, oracleKeyRemoteReport, conf.AbsOraclePubKeyPath()); err != nil {
-			log.Errorf("failed to save oracle pub key and its remote report: %v", err)
-			return err
-		}
+			// seal and store oracle private key
+			if err := sgx.SealToFile(oraclePrivKey.Serialize(), oraclePrivKeyPath); err != nil {
+				log.Errorf("failed to write %s: %v", oraclePrivKeyPath, err)
+				return err
+			}
 
-		return nil
-	},
+			// generate oracle key remote report
+			oraclePubKey := oraclePrivKey.PubKey().SerializeCompressed()
+			oracleKeyRemoteReport, err := sgx.GenerateRemoteReport(oraclePubKey)
+			if err != nil {
+				log.Errorf("failed to generate remote report of oracle key: %v", err)
+				return err
+			}
+
+			// store oracle pub key and its remote report to a file
+			if err := storeOraclePubKey(oraclePubKey, oracleKeyRemoteReport, conf.AbsOraclePubKeyPath()); err != nil {
+				log.Errorf("failed to save oracle pub key and its remote report: %v", err)
+				return err
+			}
+
+			return nil
+		},
+	}
+	return cmd
 }
 
 // storeOraclePubKey stores base64-encoded oracle public key and its remote report
--- panacea/query_client_test.go
+++ panacea/query_client_test.go
@@ -4,17 +4,12 @@ import (
 	"context"
 	"encoding/hex"
 	"fmt"
-	"os"
-	"path/filepath"
-	"sync"
-	"testing"
-
 	"github.com/cosmos/cosmos-sdk/types/bech32"
 	"github.com/medibloc/panacea-doracle/config"
 	"github.com/stretchr/testify/require"
+	"sync"
+	"testing"
 )
-
-// All the tests can only work in sgx environment, so the tests are commented out.
 
 // Test for GetAccount function.
 func TestGetAccount(t *testing.T) {
@@ -27,15 +22,30 @@ func TestGetAccount(t *testing.T) {
 		TrustedBlockHeight: 99,
 		TrustedBlockHash:   hash,
 	}
-	userHomeDir, err := os.UserHomeDir()
-	require.NoError(t, err)
 
-	homeDir := filepath.Join(userHomeDir, ".doracle")
-	conf, err := config.ReadConfigTOML(filepath.Join(homeDir, "config.toml"))
-	require.NoError(t, err)
+	conf := &config.Config{
+		BaseConfig: config.BaseConfig{
+			LogLevel:          "",
+			OracleMnemonic:    "",
+			ListenAddr:        "",
+			Subscriber:        "",
+			DataDir:           "data",
+			OraclePrivKeyFile: "oracle_priv_key.sealed",
+			OraclePubKeyFile:  "oracle_pub_key.json",
+			NodePrivKeyFile:   "node_priv_key.sealed",
+		},
+		Panacea: config.PanaceaConfig{
+			GRPCAddr:                "https://grpc.gopanacea.org:443",
+			RPCAddr:                 "https://rpc.gopanacea.org:443",
+			ChainID:                 "panacea-3",
+			DefaultGasLimit:         200000,
+			DefaultFeeAmount:        "1000000umed",
+			LightClientPrimaryAddr:  "https://rpc.gopanacea.org:443",
+			LightClientWitnessAddrs: []string{"https://rpc.gopanacea.org:443"},
+		},
+	}
 
 	queryClient, err := NewQueryClient(ctx, conf, trustedBlockinfo)
-
 	require.NoError(t, err)
 
 	mediblocLimitedAddress := "panacea1ewugvs354xput6xydl5cd5tvkzcuymkejekwk3"
@@ -46,11 +56,66 @@ func TestGetAccount(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, mediblocLimitedAddress, address)
+}
+
+// Test for LoadQueryClient function.
+func TestLoadQueryClient(t *testing.T) {
+	hash, err := hex.DecodeString("3531F0F323110AA7831775417B9211348E16A29A07FBFD46018936625E4E5492")
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	trustedBlockinfo := TrustedBlockInfo{
+		TrustedBlockHeight: 99,
+		TrustedBlockHash:   hash,
+	}
+
+	conf := &config.Config{
+		BaseConfig: config.BaseConfig{
+			LogLevel:          "",
+			OracleMnemonic:    "",
+			ListenAddr:        "",
+			Subscriber:        "",
+			DataDir:           "data",
+			OraclePrivKeyFile: "oracle_priv_key.sealed",
+			OraclePubKeyFile:  "oracle_pub_key.json",
+			NodePrivKeyFile:   "node_priv_key.sealed",
+		},
+		Panacea: config.PanaceaConfig{
+			GRPCAddr:                "https://grpc.gopanacea.org:443",
+			RPCAddr:                 "https://rpc.gopanacea.org:443",
+			ChainID:                 "panacea-3",
+			DefaultGasLimit:         200000,
+			DefaultFeeAmount:        "1000000umed",
+			LightClientPrimaryAddr:  "https://rpc.gopanacea.org:443",
+			LightClientWitnessAddrs: []string{"https://rpc.gopanacea.org:443"},
+		},
+	}
+
+	queryClient, err := NewQueryClient(ctx, conf, trustedBlockinfo)
+
+	require.NoError(t, err)
+
+	_, err = queryClient.lightClient.LastTrustedHeight()
+	require.NoError(t, err)
+
+	_, err = NewQueryClient(ctx, conf, trustedBlockinfo)
+	require.Error(t, err)
 
 	err = queryClient.Close()
 	require.NoError(t, err)
+
+	queryClient, err = LoadQueryClient(ctx, conf)
+	require.NoError(t, err)
+
+	_, err = queryClient.lightClient.LastTrustedHeight()
+	require.NoError(t, err)
+
+	err = queryClient.Close()
+	require.NoError(t, err)
+
 }
 
+// Test that calls multiple GetAddress at the same time
 func TestMultiGetAddress(t *testing.T) {
 
 	hash, err := hex.DecodeString("3531F0F323110AA7831775417B9211348E16A29A07FBFD46018936625E4E5492")
@@ -61,11 +126,29 @@ func TestMultiGetAddress(t *testing.T) {
 		TrustedBlockHeight: 99,
 		TrustedBlockHash:   hash,
 	}
-	userHomeDir, err := os.UserHomeDir()
-	require.NoError(t, err)
 
-	homeDir := filepath.Join(userHomeDir, ".doracle")
-	conf, err := config.ReadConfigTOML(filepath.Join(homeDir, "config.toml"))
+	conf := &config.Config{
+		BaseConfig: config.BaseConfig{
+			LogLevel:          "",
+			OracleMnemonic:    "",
+			ListenAddr:        "",
+			Subscriber:        "",
+			DataDir:           "data",
+			OraclePrivKeyFile: "oracle_priv_key.sealed",
+			OraclePubKeyFile:  "oracle_pub_key.json",
+			NodePrivKeyFile:   "node_priv_key.sealed",
+		},
+		Panacea: config.PanaceaConfig{
+			GRPCAddr:                "https://grpc.gopanacea.org:443",
+			RPCAddr:                 "https://rpc.gopanacea.org:443",
+			ChainID:                 "panacea-3",
+			DefaultGasLimit:         200000,
+			DefaultFeeAmount:        "1000000umed",
+			LightClientPrimaryAddr:  "https://rpc.gopanacea.org:443",
+			LightClientWitnessAddrs: []string{"https://rpc.gopanacea.org:443"},
+		},
+	}
+
 	require.NoError(t, err)
 
 	queryClient, err := NewQueryClient(ctx, conf, trustedBlockinfo)
@@ -118,47 +201,86 @@ func TestMultiGetAddress(t *testing.T) {
 	}()
 	wg.Wait()
 
-	err = queryClient.Close()
-	require.NoError(t, err)
-
 }
 
-func TestLoadQueryClient(t *testing.T) {
-	hash, err := hex.DecodeString("3531F0F323110AA7831775417B9211348E16A29A07FBFD46018936625E4E5492")
-	require.NoError(t, err)
-	ctx := context.Background()
+// The test below is commented out because it is a test targeting local panacea.
 
-	userHomeDir, err := os.UserHomeDir()
-	require.NoError(t, err)
-
-	homeDir := filepath.Join(userHomeDir, ".doracle")
-	conf, err := config.ReadConfigTOML(filepath.Join(homeDir, "config.toml"))
-	require.NoError(t, err)
-
-	trustedBlockinfo := TrustedBlockInfo{
-		TrustedBlockHeight: 99,
-		TrustedBlockHash:   hash,
-	}
-
-	queryClient, err := NewQueryClient(ctx, conf, trustedBlockinfo)
-	require.NoError(t, err)
-
-	_, err = queryClient.lightClient.LastTrustedHeight()
-	require.NoError(t, err)
-
-	_, err = NewQueryClient(ctx, conf, trustedBlockinfo)
-	require.Error(t, err)
-
-	err = queryClient.Close()
-	require.NoError(t, err)
-
-	queryClient, err = LoadQueryClient(ctx, conf)
-	require.NoError(t, err)
-
-	_, err = queryClient.lightClient.LastTrustedHeight()
-	require.NoError(t, err)
-
-	err = queryClient.Close()
-	require.NoError(t, err)
-
-}
+// Test for GetOracleRegistration function.
+//func TestGetOracleRegistration(t *testing.T) {
+//	hash, err := hex.DecodeString("93EF7A66EE58C1063B13CE408D0D850CE2B4E396D366C92BD5DB1BBF9FA1C4BC")
+//	require.NoError(t, err)
+//	ctx := context.Background()
+//
+//	trustedBlockinfo := TrustedBlockInfo{
+//		TrustedBlockHeight: 10,
+//		TrustedBlockHash:   hash,
+//	}
+//
+//	conf := &config.Config{
+//		Panacea: config.PanaceaConfig{
+//			ChainID:                 "local",
+//			RPCAddr:                 "tcp://127.0.0.1:26657",
+//			LightClientPrimaryAddr:  "tcp://127.0.0.1:26657",
+//			LightClientWitnessAddrs: []string{"tcp://127.0.0.1:26657"},
+//			GRPCAddr:                "127.0.0.1:9090",
+//		},
+//	}
+//
+//	grpcClient, err := NewGrpcClient(conf)
+//	require.NoError(t, err)
+//	queryClient, err := NewQueryClient(ctx, conf, trustedBlockinfo)
+//	require.NoError(t, err)
+//
+//	mnemonic := "genre cook grace border huge learn collect suffer head casino trial elegant hood check organ galaxy athlete become super typical bulk describe scout fetch"
+//
+//	oracleAccount, err := NewOracleAccount(mnemonic, 0, 0)
+//	require.NoError(t, err)
+//
+//	// generate node key and its remote report
+//	nodePubKey, nodePubKeyRemoteReport, err := generateNodeKey()
+//	require.NoError(t, err)
+//
+//	report, _ := enclave.VerifyRemoteReport(nodePubKeyRemoteReport)
+//	uniqueID := hex.EncodeToString(report.UniqueID)
+//
+//	// sign and broadcast to Panacea
+//	msgRegisterOracle := oracletypes.NewMsgRegisterOracle(uniqueID, oracleAccount.GetAddress(), nodePubKey, nodePubKeyRemoteReport, trustedBlockinfo.TrustedBlockHeight, trustedBlockinfo.TrustedBlockHash)
+//
+//	txBuilder := NewTxBuilder(*queryClient)
+//
+//	defaultFeeAmount, _ := sdk.ParseCoinsNormalized("1500000umed")
+//	txBytes, err := txBuilder.GenerateSignedTxBytes(oracleAccount.GetPrivKey(), 300000, defaultFeeAmount, msgRegisterOracle)
+//	require.NoError(t, err)
+//
+//	_, err = grpcClient.BroadcastTx(txBytes)
+//	require.NoError(t, err)
+//
+//	fmt.Println("register-oracle transaction succeed")
+//
+//	oracleRegistrationFromGrpc, err := grpcClient.GetOracleRegistration(oracleAccount.GetAddress(), uniqueID)
+//	require.NoError(t, err)
+//
+//	fmt.Println("unique ID1:", oracleRegistrationFromGrpc.UniqueId)
+//
+//	oracleRegistration, err := queryClient.GetOracleRegistration(oracleAccount.GetAddress(), uniqueID)
+//	require.NoError(t, err)
+//
+//	fmt.Println("unique ID2:", oracleRegistration.UniqueId)
+//
+//	require.EqualValues(t, oracleRegistrationFromGrpc.UniqueId, oracleRegistration.UniqueId)
+//}
+//
+//func generateNodeKey() ([]byte, []byte, error) {
+//	nodePrivKey, err := crypto.NewPrivKey()
+//	if err != nil {
+//		return nil, nil, err
+//	}
+//	nodePubKey := nodePrivKey.PubKey().SerializeCompressed()
+//	oraclePubKeyHash := sha256.Sum256(nodePubKey)
+//	nodeKeyRemoteReport, err := sgx.GenerateRemoteReport(oraclePubKeyHash[:])
+//	if err != nil {
+//		return nil, nil, err
+//	}
+//
+//	return nodePubKey, nodeKeyRemoteReport, nil
+//}

8ab8d006f95dc7e118d8a7c68f923212901eaca2
audtlr24 <audtlr24@gmail.com>
2022-09-13 16:47:37 +0900

    feat: add trustedBlock info at gen-oracle-key when creating the genesis oracle

--- client/flags/flags.go
+++ client/flags/flags.go
@@ -2,6 +2,7 @@ package flags
 
 const (
 	FlagHome               = "home"
+	FlagIsGenesisOracle    = "is-genesis-oracle"
 	FlagTrustedBlockHeight = "trusted-block-height"
 	FlagTrustedBlockHash   = "trusted-block-hash"
 	FlagAccNum             = "acc-num"

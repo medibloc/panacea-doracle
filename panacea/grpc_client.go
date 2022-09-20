02fd55b30b8537c70bb658211a7c922b9305c54b
hansol-medi <hansol@medibloc.org>
2022-09-20 14:22:30 +0900

    fix: add tls certificate in connection with https endpoint

--- panacea/grpc_client.go
+++ panacea/grpc_client.go
@@ -3,11 +3,13 @@ package panacea
 import (
 	"context"
 	"fmt"
+	"net/url"
 
 	"github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/medibloc/panacea-doracle/config"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type GrpcClient struct {
@@ -16,7 +18,27 @@ type GrpcClient struct {
 
 func NewGrpcClient(conf *config.Config) (*GrpcClient, error) {
 	log.Infof("dialing to Panacea gRPC endpoint: %s", conf.Panacea.GRPCAddr)
-	conn, err := grpc.Dial(conf.Panacea.GRPCAddr, grpc.WithInsecure())
+
+	//var targetUrl string
+	parsedUrl, err := url.Parse(conf.Panacea.GRPCAddr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse gRPC endpoint. please use absolute URL (scheme://host:port): %w", err)
+	}
+
+	var cred grpc.DialOption
+
+	if parsedUrl.Scheme == "https" {
+		tlsConfig, err := config.CreateTLSConfig()
+		if err != nil {
+			return nil, fmt.Errorf("failed to create TLS cofig: %w", err)
+		}
+		cred = grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig))
+		log.Infof("tls connection! %v", parsedUrl.Host)
+	} else {
+		cred = grpc.WithInsecure()
+	}
+
+	conn, err := grpc.Dial(parsedUrl.Host, cred)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to Panacea: %w", err)
 	}
